a7d386747145becabedc44c1283be80afa8d3470
suhaillahmad <suhaillahmadd0@gmail.com>
2022-10-08 14:39:59 +0530

    Added left-RightMovement

--- script.js
+++ script.js
@@ -1,1 +1,37 @@
+const canvas = document.getElementById("myCanvas");
+const ctx = canvas.getContext("2d");
 
+canvas.width = window.innerHeight;
+canvas.height = window.innerHeight;
+
+let x = 0;
+let y = 650;
+
+function animation() {
+  ctx.fillStyle = "yellow";
+  ctx.fillRect(x, y, 100, 100);
+  requestAnimationFrame(animation);
+}
+
+animation();
+
+function moveDown() {
+  ctx.clearRect(x, y, 100, 100);
+  y += 50;
+  console.log(y);
+}
+
+function move(event) {
+  ctx.clearRect(x, y, 100, 100);
+  if (event.key === "d") {
+    x += 5;
+  }
+  if (event.key === "a" && x > 0) {
+    x -= 5;
+  }
+  if (event.key === "w") {
+    y -= 50;
+  }
+}
+
+addEventListener("keypress", move);
