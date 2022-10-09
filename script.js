82a5c197bed68f4a429f0f105f16881f97c7d786
Madhur <madhur12a16072@gmail.com>
2022-10-09 22:05:48 +0530

    Added Moment Boundation

--- script.js
+++ script.js
@@ -1,11 +1,116 @@
 
+// const canvas = document.getElementById("myCanvas");
+// const ctx = canvas.getContext("2d");
+
+// canvas.width = window.innerWidth;
+// canvas.height = window.innerHeight;
+
+// let x = 100;
+// let y = 100;
+// let xv = 0;
+// let yv = 0;
+// let platX = 0;
+// let platY = 100;
+// let flag = true;
+// const backHeight = window.innerHeight;
+// const backWidth = window.innerWidth;
+
+// const groundGravity = 0.5;
+
+// const image = new Image();
+// image.src = "img/backgroundimg.png";
+
+// const player_image = new Image();
+// player_image.src = "img/player_movement_sprite.png";
+// const player_sprite_width = 25;
+// const player_sprite_height = 35;
+
+// let a = 0;
+// let b = 0;
+// let framex = 0;
+// let framey = 0;
+// function drawPlatform(a, b) {
+//   ctx.fillStyle = "blue";
+//   ctx.drawImage(image, a, b, backWidth * 2.7, backHeight * 0.4, platX, platY, backWidth * 7, backHeight * .94);
+// }
+
+// function drawPlayer() {
+//   ctx.drawImage(player_image, framex * player_sprite_width, 114, player_sprite_width, player_sprite_height, x, y, 60, 60);
+//   // ctx.fillStyle = "yellow";
+//   // ctx.fillRect(x, y, 100, 100);
+// }
+
+// function update() {
+//   drawPlatform(a, b);
+//   drawPlayer();
+
+//   x += xv;
+//   y += yv;
+//   yv += groundGravity;
+//   if (x >= 500) {
+//     a++;
+//     x = 499;
+//   }
+//   if (y + yv + 100 <= canvas.height / 2 + 75) yv += groundGravity;
+//   else {
+//     yv = 0;
+//     flag = true;
+//   }
+// }
+
+// function move(event) {
+//   if (event.key === "d" && x < 400) {
+//     if (framex < 2)
+//       framex++;
+//     else
+//       framex = 0;
+//     xv = 2;
+//   }
+//   if (event.key === "a" && x > 100) {
+//     xv = -2;
+//   }
+//   if (event.key === "w") {
+//     if (flag) {
+//       yv -= 20;
+//       flag = false;
+//     }
+//   }
+// }
+// function stop(event) {
+//   if (event.key === "d") {
+//     xv = 0;
+//   }
+//   if (event.key === "a") {
+//     xv = 0;
+//   }
+// }
+
+// addEventListener("keydown", move);
+// addEventListener("keyup", stop);
+
+// function animation() {
+//   requestAnimationFrame(animation);
+//   ctx.clearRect(0, 0, canvas.width, canvas.height);
+//   update();
+//   if (x > 800 || x < 100) {
+//     xv = 0;
+//   }
+//   if (x > 800) {
+//     platX -= 10;
+//   }
+//   if (x < 100) {
+//     platX += 10;
+//   }
+// }
+// animation();
+
 const canvas = document.getElementById("myCanvas");
 const ctx = canvas.getContext("2d");
 
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-let x = 100;
+let x = 200;
 let y = 100;
 let xv = 0;
 let yv = 0;
@@ -17,6 +122,7 @@ const backWidth = window.innerWidth;
 
 const groundGravity = 0.5;
 
+
 const image = new Image();
 image.src = "img/backgroundimg.png";
 
@@ -24,27 +130,34 @@ const player_image = new Image();
 player_image.src = "img/player_movement_sprite.png";
 const player_sprite_width = 25;
 const player_sprite_height = 35;
-
+let a = 0;
+let b = 0;
 let framex = 0;
 let framey = 0;
-function drawPlatform() {
+function drawPlatform(a, b) {
   ctx.fillStyle = "blue";
-  ctx.drawImage(image, platX, platY, backWidth * 7, backHeight * .94);
+  ctx.drawImage(image, a, b, backWidth * 2.4, backHeight * 0.4, platX, platY, backWidth * 7, backHeight * .94);
 }
 
 function drawPlayer() {
-  ctx.drawImage(player_image, framex * player_sprite_width, 114, player_sprite_width, player_sprite_height, x, y, 60, 60);
-  // ctx.fillStyle = "yellow";
-  // ctx.fillRect(x, y, 100, 100);
+  ctx.drawImage(player_image, framex * player_sprite_width, 114, player_sprite_width, player_sprite_height, x, y, 80, 80);
 }
 
 function update() {
+  drawPlatform(a, b);
   drawPlayer();
   x += xv;
   y += yv;
+  console.log(x, " ", y)
   yv += groundGravity;
-
-  if (y + yv + 100 <= canvas.height / 2 + 75) yv += groundGravity;
+  if (x >= 550) {
+    a++;
+    x = 549;
+  }
+  if (x <= 110) {
+    x = 110;
+  }
+  if (y + yv + 100 <= canvas.height / 2 + 15) yv += groundGravity;
   else {
     yv = 0;
     flag = true;
@@ -52,14 +165,14 @@ function update() {
 }
 
 function move(event) {
-  if (event.key === "d" && x < 400) {
+  if (event.key === "d") {
     if (framex < 2)
       framex++;
     else
       framex = 0;
     xv = 2;
   }
-  if (event.key === "a" && x > 100) {
+  if (event.key === "a" && x > 350) {
     xv = -2;
   }
   if (event.key === "w") {
@@ -84,31 +197,7 @@ addEventListener("keyup", stop);
 function animation() {
   requestAnimationFrame(animation);
   ctx.clearRect(0, 0, canvas.width, canvas.height);
-  drawPlatform();
-  update();
-
-  if (
-    y + 100 <= platY &&
-    y + 100 + yv >= platY &&
-    x + 100 >= platX &&
-    x <= platX + 400
-  ) {
-    yv = 0;
-  }
-  if (x > 800 || x < 100) {
-    xv = 0;
-  }
-  if (x > 800) {
-    platX -= 10;
-  }
-  if (x < 100) {
-    platX += 10;
-  }
+  update()
 }
 
 animation();
-
-
-
-
-
