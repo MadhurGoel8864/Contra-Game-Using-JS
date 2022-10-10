d223a9cdbd7dc6d80be62d3a8cd15557c92f3f3a
suhaillahmad <suhaillahmadd0@gmail.com>
2022-10-10 11:10:57 +0530

    Added Track Array

--- script.js
+++ script.js
@@ -16,6 +16,8 @@ let platY = 100;
 let flag = true;
 const backHeight = window.innerHeight;
 const backWidth = window.innerWidth;
+shiftTrack = 0;
+shiftTrackBy = 6;
 
 const groundGravity = 0.5;
 
@@ -38,19 +40,186 @@ let facing = 1;
 let rotate_angle = 180;
 let cnt = 0;
 
-function drawPlatform(a, b) {
-  ctx.fillStyle = "blue";
-  ctx.drawImage(
-    image,
-    a,
-    b,
-    backWidth * 2.4,
-    backHeight * 0.4,
-    platX,
-    platY,
-    backWidth * 7,
-    backHeight * 0.94
-  );
+let trackImage = {
+  road: { sx: 100, sy: 100, sw: 60, sh: 60 },
+  grass: { sx: 100, sy: 65, sw: 70, sh: 30 },
+  bridgeStart: { sx: 1055, sy: 105, sw: 60, sh: 30 },
+  bridge: { sx: 1075, sy: 105, sw: 60, sh: 30 },
+  bridgeEnd: { sx: 1125, sy: 105, sw: 60, sh: 30 },
+  enemyBot: { sx: 320, sy: 140, sw: 30, sh: 25 },
+  enemyBot2: { sx: 1125, sy: 105, sw: 60, sh: 30 },
+  waterTop: { sx: 1180, sy: 185, sw: 60, sh: 30 },
+  water: { sx: 1110, sy: 185, sw: 60, sh: 30 },
+  wall: { sx: 1188, sy: 120, sw: 60, sh: 60 },
+};
+
+track = [
+  [
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0,
+  ],
+  [
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0,
+  ],
+  [
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0,
+  ],
+  [
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0,
+  ],
+  [
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0,
+  ],
+  [
+    2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 0, 0, 0, 0, 0, 0, 2, 2, 2, 2, 2, 2, 2,
+    2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2,
+    2, 2, 2, 2, 2, 2, 2, 2,
+  ],
+  [
+    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 3, 4, 4, 4, 4, 5, 1, 1, 1, 1, 1, 1, 1,
+    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+    1, 1, 1, 1, 1, 1, 1, 1,
+  ],
+  [
+    9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 8, 8, 8, 8, 8, 8, 9, 9, 9, 9, 9, 9, 9,
+    9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9,
+    9, 9, 9, 9, 9, 9, 9, 9,
+  ],
+  [
+    9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 8, 8, 8, 8, 8, 8, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0,
+  ],
+  [
+    9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 8, 8, 8, 8, 8, 8, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0,
+  ],
+  [
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0,
+  ],
+  [
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0,
+  ],
+];
+
+function moveForward() {
+  for (let y = 0; y < this.track.length; y++) {
+    if (shiftTrack >= 60) {
+      console.log();
+      track[y].shift();
+    }
+  }
+  if (shiftTrack < 60) {
+    shiftTrack += shiftTrackBy;
+  } else {
+    shiftTrack = 0;
+  }
+}
+
+function drawPlatform() {
+  for (let y = 0; y < track.length; y++) {
+    for (let x = 0; x < track[y].length; x++) {
+      if (this.track[y][x] != 0) {
+        let trackPosition = {
+          x: x * 60 - shiftTrack,
+          y: y * 60,
+        };
+
+        switch (track[y][x]) {
+          case 1: {
+            drawTrack({
+              trackPosition,
+              trackDetail: { image: "background", trackSource: "road" },
+            });
+            break;
+          }
+          case 2: {
+            drawTrack({
+              trackPosition,
+              trackDetail: { image: "background", trackSource: "grass" },
+            });
+            break;
+          }
+          case 3: {
+            drawTrack({
+              trackPosition,
+              trackDetail: { image: "background", trackSource: "bridgeStart" },
+            });
+            break;
+          }
+          case 4: {
+            drawTrack({
+              trackPosition,
+              trackDetail: { image: "background", trackSource: "bridge" },
+            });
+            break;
+          }
+          case 5: {
+            drawTrack({
+              trackPosition,
+              trackDetail: { image: "background", trackSource: "bridgeEnd" },
+            });
+            break;
+          }
+          case 6: {
+            drawTrack({
+              trackPosition,
+              trackDetail: { image: "background", trackSource: "enemyBot" },
+            });
+            break;
+          }
+          case 7: {
+            drawTrack({
+              trackPosition,
+              trackDetail: { image: "background", trackSource: "enemyBot2" },
+            });
+            break;
+          }
+          case 8: {
+            drawTrack({
+              trackPosition,
+              trackDetail: { image: "background", trackSource: "water" },
+            });
+            break;
+          }
+          case 9: {
+            drawTrack({
+              trackPosition,
+              trackDetail: { image: "background", trackSource: "wall" },
+            });
+            break;
+          }
+        }
+      }
+    }
+  }
+  function drawTrack({ trackPosition, trackDetail }) {
+    ctx.drawImage(
+      image,
+      trackImage[trackDetail.trackSource].sx,
+      trackImage[trackDetail.trackSource].sy,
+      trackImage[trackDetail.trackSource].sw,
+      trackImage[trackDetail.trackSource].sh,
+      trackPosition.x,
+      trackPosition.y,
+      60,
+      60
+    );
+  }
 }
 
 function drawPlayer() {
@@ -80,62 +249,18 @@ function drawPlayer() {
     );
   }
 }
-let enemyFrame = 0;
-
-function drawEnemy() {
-  ctx.drawImage(
-    reversed_image,
-    enemyFrame * reversed_player_sprite_width,
-    1,
-    reversed_player_sprite_width,
-    reversed_player_sprite_height,
-    ex,
-    ey,
-    80,
-    80
-  );
-}
-
-function drawEnemy2() {
-  ctx.drawImage(
-    reversed_image,
-    enemyFrame * reversed_player_sprite_width,
-    1,
-    reversed_player_sprite_width,
-    reversed_player_sprite_height,
-    ex2,
-    ey,
-    80,
-    80
-  );
-}
-
-function drawEnemy3() {
-  ctx.drawImage(
-    reversed_image,
-    enemyFrame * reversed_player_sprite_width,
-    1,
-    reversed_player_sprite_width,
-    reversed_player_sprite_height,
-    ex1,
-    ey,
-    80,
-    80
-  );
-}
 
 function create_fireball() {}
 
 function update() {
-  drawPlatform(a, b);
+  drawPlatform();
   drawPlayer();
   x += xv;
   y += yv;
-  // console.log(x, " ", y)
   yv += groundGravity;
   if (x >= 550) {
-    a++;
     x = 549;
+    moveForward();
   }
   if (x <= 110) {
     x = 110;
@@ -145,29 +270,19 @@ function update() {
     yv = 0;
     flag = true;
   }
-  if (enemyFrame < 2 && cnt == 5) {
-    enemyFrame++;
-    cnt = 0;
-  } else {
-    enemyFrame = 0;
-  }
-  cnt++;
-  ex--;
-  ex1--;
-  ex2--;
 }
 
 function move(event) {
   if (event.key === "d") {
     facing = 1;
 
-    xv = 2;
+    xv = 3;
   }
   if (event.key === "a") {
     if (framex < 2) framex++;
     else framex = 0;
     facing = 0;
-    xv = -2;
+    xv = -3;
   }
   if (event.key === "w") {
     if (flag) {
@@ -192,8 +307,6 @@ function animation() {
   requestAnimationFrame(animation);
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   update();
-  drawEnemy();
-  drawEnemy2();
 }
 
 animation();
